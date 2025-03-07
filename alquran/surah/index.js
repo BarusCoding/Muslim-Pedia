38a41074aef56310fc55a72a015cd8951581001d
Rasid Barus <barus.soleh1997@gmail.com>
2025-03-07 10:17:43 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
   // Request ke api dengan parameter yang sudah diambil
   $.ajax({
-    url: `https://api.quran.gading.dev/surah/${nomerSurat}`,
+    url: 'https://quran-api-id.vercel.app//surah/${nomerSurat}`,
     success: (results) => {
       // Mengecek apakah yang dihasilkan dari request API adalah JSON atau String
       results = typeof results == "string" ? JSON.parse(results) : results;
